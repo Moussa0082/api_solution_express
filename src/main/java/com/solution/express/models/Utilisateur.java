168ab1a22f2124860c59dcfbc0931ea263e8cf32
MOUSSA BANE <bane8251@gmail.com>
2023-10-31 23:54:46 +0000

    premier commits

---
+++ src/main/java/com/solution/express/models/Utilisateur.java
@@ -0,0 +1,18 @@
+import javax.annotation.processing.Generated;
+
+@Entity
+@Data
+public class Utilisateur {
+
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // private int idUtilisateur;
+    // @Column(nullable = false)
+    // private String nom;
+    // private String prenom;
+    // @Column(nullable = false, unique = true)
+    // @Email(message = "Email incorrect !")
+    // private String email;
+    // private String motDePasse;
+
+    
+}
